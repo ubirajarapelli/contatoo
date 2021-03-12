00068ea7be0645af0fc98b2193e9afb0496e55cf
ubirajarapelli <ubirajaracpelli@gmail.com>
2021-03-11 22:08:39 -0300

    Move Files to root before publish

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  verbose: true,
-  clearMocks: true,
-  setupFilesAfterEnv: ['regenerator-runtime/runtime'],
-  testPathIgnorePatterns: [
-    "/node_modules/",
-  ],
-};
